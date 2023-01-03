6942b0a12f4d6b5316f06579cbf63b0107bce8f8
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-01-03 16:45:54 -0300

    feat: adicionado react markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import reactLogo from './assets/react.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkBreaks from 'remark-breaks';
 
 function App() {
   const [data, setData] = useState([]);
@@ -20,8 +22,12 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Quadidade de testes criados: {data.length}</h1>
       {data.map((testCase: any) => (
-        <p>{testCase.testTitle}</p>
+        <ReactMarkdown
+          children={testCase.testTitle}
+          remarkPlugins={[remarkBreaks]}
+        />
       ))}
     </div>
   );

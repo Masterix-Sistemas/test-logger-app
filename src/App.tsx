b75a3276761e4dc83ed32d40b41934977f1188b2
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-01-03 23:28:46 -0300

    feat: adicionado componente 'sidebar'

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import reactLogo from './assets/react.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
-import remarkBreaks from 'remark-breaks';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Sidebar from './pages/global/Sidebar';
+import theme from './theme';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  function getData() {
-    return fetch('http://localhost:8080/api/testCase')
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-      });
-  }
-
-  useEffect(() => {
-    getData();
-    console.log(data);
-  }, []);
-
   return (
-    <div className="App">
-      <h1>Quadidade de testes criados: {data.length}</h1>
-      {data.map((testCase: any) => (
-        <ReactMarkdown
-          children={testCase.testTitle}
-          remarkPlugins={[remarkBreaks]}
-        />
-      ))}
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="app">
+        <Sidebar />
+        <main className="content"></main>
+      </div>
+    </ThemeProvider>
   );
 }
 
